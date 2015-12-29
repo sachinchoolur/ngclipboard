636e1c5a5b1c3d6cd2df930eee103feaecb741b6
Idan Entin <idanen@gmail.com>
2015-12-29 15:13:25 +0200

    Using directive's element instead of adding arbitray id and selecting it

--- src/ngclipboard.js
+++ src/ngclipboard.js
@@ -8,14 +8,7 @@
                 ngclipboardError: '&'
             },
             link: function(scope, element) {
-
-                var _id = element.attr('id');
-                if (!_id) {
-                    element.attr('id', 'ngclipboard' + Date.now());
-                    _id = element.attr('id');
-                }
-
-                var clipboard = new Clipboard('#' + _id);
+                var clipboard = new Clipboard(element[0]);
 
                 clipboard.on('success', function(e) {
                     scope.ngclipboardSuccess({
